ef8b0a7317395c26f0b99fde0cd8cdb8418d0065
Larry <melarry46@gmail.com>
2025-05-27 10:23:39 +0300

    orm model for projects

--- Models/user.py
+++ Models/user.py
@@ -1,3 +1,11 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from models.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column (String, nullable=False, unique=True)
+    email = Column(String, nullable=False, unique=True)
+
+    projects = relationship("Project", back_populates="user")
