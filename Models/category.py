b90a983eff59e7a99dceca13c0487e62f52ee133
Larry <melarry46@gmail.com>
2025-05-27 10:28:44 +0300

    relationship

--- Models/category.py
+++ Models/category.py
@@ -6,3 +6,5 @@ class Category(Base):
     __tablename__ = "categories"
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True)
+
+    tasks = relationship("Task", back_populates="category")
