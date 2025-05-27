7ae0ea98ac149e508194d21b3195d3ade03ec400
Larry <melarry46@gmail.com>
2025-05-27 10:15:02 +0300

    initialize database and make all tables

--- database/setup.py
+++ database/setup.py
@@ -3,3 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from models.base import Base
 
 engine = create_engine("sqlite:///taskmanager.db")
+SessionLocal = sissionmaker(bind=engine)
+
+def init_db():
+     import models.user
+     import models.project
+     import models.task
+     import models.category
+     Base.metadata.create_all(bind=engine)
