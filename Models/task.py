6a78601a6cdf1b60e5dc8bdbd7e4937ebfa4607f
Larry <melarry46@gmail.com>
2025-05-27 10:26:17 +0300

    orm model for tasks

---
+++ Models/task.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
+from sqlalchemy.orm import relationship
+from models.base import Base
+
+class Task(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    due_date = Column(Date)
+    status = Column(String, default="Pending")
+    project_id = Column(Integer, ForeignKey("projects.id"))
+    category_id = Column(Integer, ForeignKey("categories.id"))
