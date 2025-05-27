a1f185877118ffb0a3cb048457218a54c0d80c00
Larry <melarry46@gmail.com>
2025-05-27 10:28:07 +0300

    orm models for categories

---
+++ Models/category.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.base import Base
+
+class Category(Base):
+    __tablename__ = "categories"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
